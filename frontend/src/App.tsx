305c31a41ec6b5b1049afbcb1c99813d462a370b
lopess <feikaua03@gmail.com>
2026-09-22 17:34:36 -0300

    Remove login gate from home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
-import AuthGate from "@/components/AuthGate";
 
 // One <Route> per page in src/pages; BrowserRouter already wraps this in main.tsx.
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<AuthGate><Home /></AuthGate>} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
